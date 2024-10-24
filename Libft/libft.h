7e8e91bbff12d3666c6f2e76ef4107f036bb954a
Georgii Tretiakov <george22021988@gmail.com>
2024-10-24 17:44:42 +0100

    Add files via upload

---
+++ Libft/libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtretiak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 13:30:34 by gtretiak          #+#    #+#             */
+/*   Updated: 2024/10/24 17:38:40 by gtretiak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+OBJECTS = *.o
+SRCS = ft_putchar.c ft_putstr.c ft_strcmp.c ft_strlen.c ft_swap.c
+HEAD = libft.h
+NAME = libft.a
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -I
+
